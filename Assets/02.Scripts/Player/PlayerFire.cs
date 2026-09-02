c75fab10d640920f69ee7e46b28f7b376d2d6047
Jae-hyun Song <ccwcs@naver.com>
2026-09-02 14:47:38 +0900

    feat: 총알 여러 발 동시 발사

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -8,6 +8,8 @@ public class PlayerFire : MonoBehaviour
     public GameObject BulletPrefab = null;
     // - 생성 위치(총구)
     public Transform FirePoint = null;
+    public int NumOfBulletFireOnce = 0;
+    public float DistanceBetweenFiredBullets = 0;
     
     void Update()
     {
@@ -16,8 +18,14 @@ public class PlayerFire : MonoBehaviour
         {
             // 2. 총알 프리팹을 생성한다.
             // Instantiate는 프리팹을 복사해서 (Monobehaviour를 상속받는) 게임 오브젝트를 생성하고 씬에 넣어주는 기능
-            GameObject bullet = Instantiate(BulletPrefab);
-            bullet.transform.position = FirePoint.position;
+            float lengthOfBullets = (NumOfBulletFireOnce - 1) * DistanceBetweenFiredBullets;
+            for (int i = 0; i < NumOfBulletFireOnce; i++)
+            {
+                float offsetX = -lengthOfBullets / 2 + i * DistanceBetweenFiredBullets;
+                GameObject bullet = Instantiate(BulletPrefab);
+                bullet.transform.position = FirePoint.position;
+                bullet.transform.position = (Vector2)bullet.transform.position + new Vector2(offsetX, 0);
+            }
         }
     }
 }

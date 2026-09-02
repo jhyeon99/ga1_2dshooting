ef2a46573d1fc31c43bdd574217ec69d6690fe54
Jae-hyun Song <ccwcs@naver.com>
2026-09-02 14:06:57 +0900

    feat: Bullet에 direction 변수 추가

--- Assets/02.Scripts/Player/BulletMove.cs
+++ Assets/02.Scripts/Player/BulletMove.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class BulletMove : MonoBehaviour
 {
+    public Vector2 direction = Vector2.up;
     public float speed = 0;
     
 
     void Move()
     {
-        transform.position = (Vector2)transform.position + Vector2.up * speed * Time.deltaTime;   
+        transform.position = (Vector2)transform.position + direction * speed * Time.deltaTime;   
     }
 
     void Update()

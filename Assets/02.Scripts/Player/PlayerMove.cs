05658c8fcc490d69b5a34257573ae46c430b6414
Jae-hyun Song <ccwcs@naver.com>
2026-09-02 12:03:11 +0900

    feat: 플레이어 이동 입력 계산 함수 변경

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -41,11 +41,11 @@ public class PlayerMove : MonoBehaviour
         }
         
         // 1. 키보드 입력을 받는다.
-        float h = Input.GetAxis("Horizontal");  // 키보드 왼/오른쪽 입력 상태에 따라 -1f ~ 0 ~ 1f
-        float v = Input.GetAxis("Vertical");    // 키보드 위/아래쪽 입력 상태에 따라 -1f ~ 0 ~ 1f
+        float h = Input.GetAxisRaw("Horizontal");  // 키보드 왼/오른쪽 입력 상태에 따라 -1f or 0 or 1f
+        float v = Input.GetAxisRaw("Vertical");    // 키보드 위/아래쪽 입력 상태에 따라 -1f or 0 or 1f
         
-        //float h = Input.GetAxisRaw("Horizontal");  // 키보드 왼/오른쪽 입력 상태에 따라 -1f or 0 or 1f
-        //float v = Input.GetAxisRaw("Vertical");    // 키보드 위/아래쪽 입력 상태에 따라 -1f or 0 or 1f
+        //float h = Input.GetAxis("Horizontal");  // 키보드 왼/오른쪽 입력 상태에 따라 -1f ~ 0 ~ 1f
+        //float v = Input.GetAxis("Vertical");    // 키보드 위/아래쪽 입력 상태에 따라 -1f ~ 0 ~ 1f
         
         // 2. 키보드 입력에 따라 방향을 구한다.
         // 게임에는 벡터라는 타입이 있다. 벡터는 크기와 방향을 의미한다.
@@ -54,9 +54,20 @@ public class PlayerMove : MonoBehaviour
         // 3. 방향과 속력에 따라 이동한다.
         // 속도 = 방향 * 속력
         // 매직 넘버: 보는 사람에 따라 의미가 달라질 수 있는 헷갈리는 숫자
-        Vector2 normalizedSpeed = Vector2.Normalize(direction * Speed);
-        transform.Translate(normalizedSpeed * Time.deltaTime);
+        Vector2 normalizedSpeed = Vector2.Normalize(direction);
+        transform.Translate(normalizedSpeed * Speed * Time.deltaTime);
         // deltaTime: 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 MS로 반환
+
+        if (transform.position.y > MaxPlayerY)
+        {
+            transform.position = new Vector3(transform.position.x, MaxPlayerY, transform.position.z);
+        }
+
+        if (transform.position.y < MinPlayerY)
+        {
+            transform.position = new Vector3(transform.position.x, MinPlayerY, transform.position.z);
+        }
+
         
         // 새로운 위치 = 현재 위치 + 방향 * 속력 * 시간
         // transform.position += (Vector3)direction * Speed * Time.deltaTime;

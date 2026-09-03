9d869333c97d091424ee24b54b50c6fae281a4f7
Jae-hyun Song <ccwcs@naver.com>
2026-09-03 12:49:47 +0900

    style: 변수명 변경
    코딩 컨벤션 준수

--- Assets/02.Scripts/Player/BulletMove.cs
+++ Assets/02.Scripts/Player/BulletMove.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class BulletMove : MonoBehaviour
 {
-    public Vector2 direction = Vector2.up;
-    public float speed = 0;
+    public Vector2 Direction = Vector2.up;
+    public float Speed = 0;
 
     void Move()
     {
-        transform.position = (Vector2)transform.position + direction * speed * Time.deltaTime;
+        transform.position = (Vector2)transform.position + Direction * Speed * Time.deltaTime;
     }
 
     void Update()

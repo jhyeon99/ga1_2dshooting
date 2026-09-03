e11407da906d3fde43ce6fd29dbec5daa19ae31b
Jae-hyun Song <ccwcs@naver.com>
2026-09-03 12:33:24 +0900

    style: 변수명 변경

--- Assets/02.Scripts/Player/Enemy.cs
+++ Assets/02.Scripts/Player/Enemy.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float Speed = 0;
+    public float MoveSpeed = 0;
 
     private void Update()
     {
-        transform.position = (Vector2)transform.position + Vector2.down * Speed * Time.deltaTime;
+        transform.position = (Vector2)transform.position + Vector2.down * MoveSpeed * Time.deltaTime;
     }
 }

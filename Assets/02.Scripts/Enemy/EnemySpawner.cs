0908b7cb88e4d85d7cd5467f133a69ebe8b14aab
Jae-hyun Song <ccwcs@naver.com>
2026-09-04 14:00:33 +0900

    feat: 적 타입별 스폰 확률 조정

--- Assets/02.Scripts/Enemy/EnemySpawner.cs
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -1,5 +1,12 @@
 using UnityEngine;
 
+[System.Serializable]
+public struct EnemySpawnData
+{
+    public Enemy prefab;
+    public float probability;
+}
+
 // 역할: 일정 시간마다 적을 생성해주고 싶다.
 public class EnemySpawner : MonoBehaviour
 {
@@ -7,9 +14,11 @@ public class EnemySpawner : MonoBehaviour
     // - 타이머
     [SerializeField] private float _spawnInterval;
     private float _timer = 0;
+    [SerializeField] private float _enemyMinSpawnTime = 1f;
+    [SerializeField] private float _enemyMaxSpawnTime = 3f;
 
     // - 생성할 프리팹
-    [SerializeField] private Enemy[] _enemyPrefabs;
+    [SerializeField] private EnemySpawnData[] _enemySpawnData;
 
 
     private void Update()
@@ -18,20 +27,43 @@ public class EnemySpawner : MonoBehaviour
         if (_timer > _spawnInterval)
         {
             _timer = 0;
-            _spawnInterval = Random.Range(1f, 3f); // 1 ~ 3
+            _spawnInterval = Random.Range(_enemyMinSpawnTime, _enemyMaxSpawnTime);
 
-            RandomSpawn();
+            SpawnWithProbability();
         }
     }
 
     private void RandomSpawn()
     {
-        Spawn(Random.Range(0, _enemyPrefabs.Length));
+        Spawn(Random.Range(0, _enemySpawnData.Length));
+    }
+
+    private void SpawnWithProbability()
+    {
+        float sum = 0;
+        foreach (var enemySpawnData in _enemySpawnData)
+        {
+            sum += enemySpawnData.probability;
+        }
+
+        float probability = Random.Range(0f, sum);
+
+        sum = 0;
+        for (int enemyType = 0; enemyType < _enemySpawnData.Length; enemyType++)
+        {
+            sum += _enemySpawnData[enemyType].probability;
+            if (probability <= sum)
+            {
+                Enemy enemy = Instantiate(_enemySpawnData[enemyType].prefab);
+                enemy.transform.position = transform.position;
+                break;
+            }
+        }
     }
 
     private void Spawn(int enemyType)
     {
-        Enemy enemy = Instantiate(_enemyPrefabs[enemyType]);
+        Enemy enemy = Instantiate(_enemySpawnData[enemyType].prefab);
         enemy.transform.position = transform.position;
     }
 }

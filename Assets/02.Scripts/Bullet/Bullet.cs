54b243b8007adfe39a51ff3546956969cacf91ab
Jae-hyun Song <ccwcs@naver.com>
2026-09-04 15:09:26 +0900

    feat: DeadZone 생성

--- Assets/02.Scripts/Bullet/Bullet.cs
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -16,7 +16,7 @@ public class Bullet : MonoBehaviour
         Move();
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -35,7 +35,7 @@ public abstract class Enemy : MonoBehaviour
     protected abstract void GetDirection();
 
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {

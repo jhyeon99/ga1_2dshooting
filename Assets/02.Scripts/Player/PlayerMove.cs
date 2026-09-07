7ce0da4d60e2cc7244613d0d354fc0584f82b4c0
Jae-hyun Song <ccwcs@naver.com>
2026-09-07 15:35:32 +0900

    feat: 플레이어 이동 애니메이션 적용

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -6,6 +6,7 @@ public class PlayerMove : MonoBehaviour
     // 목적: 키보드 입력에 따라서 플레이어 이동 처리를 하고 싶다.
 
     // 필요 필드:
+    private Animator _animator = null;
     public PlayerMoveCommandInvoker PlayerMoveCommandInvoker;
 
     public float Speed = 0;
@@ -18,6 +19,11 @@ public class PlayerMove : MonoBehaviour
 
     private float _inputHorizontal;
     private float _inputVertical;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     private void GetInput()
     {
@@ -58,6 +64,7 @@ public class PlayerMove : MonoBehaviour
         // 속도 = 방향 * 속력
         // 매직 넘버: 보는 사람에 따라 의미가 달라질 수 있는 헷갈리는 숫자
         Vector2 normalizedDirection = Vector2.Normalize(direction);
+        _animator.SetInteger("x", (int)normalizedDirection.x);
         Vector2 nextPlayerPosition = (Vector2)transform.position + normalizedDirection * Speed * Time.deltaTime;
         // deltaTime: 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 MS로 반환
 

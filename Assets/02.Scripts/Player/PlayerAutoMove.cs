31c3a5cc59acbab589d5e3957af7cb51d683fe18
Jae-hyun Song <ccwcs@naver.com>
2026-09-10 12:43:56 +0900

    chore: TMP 추가
    - TextMeshPro Import
    - Canvas 생성

--- Assets/02.Scripts/Player/PlayerAutoMove.cs
+++ Assets/02.Scripts/Player/PlayerAutoMove.cs
@@ -1,10 +1,26 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerAutoMove : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    private GameObject _target;
+
+    [SerializeField] private float _maxPlayerY = 0;
+
+    [SerializeField] private float _minPlayerY = 0;
 
     private void Update()
+    {
+        if (_target == null)
+        {
+            FindNearestTarget();
+        }
+
+        Move();
+    }
+
+    private void FindNearestTarget()
     {
         // 1. 타겟을 구한다.
         GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
@@ -22,17 +38,37 @@ public class PlayerAutoMove : MonoBehaviour
             {
                 // 타겟 변경
                 minDistance = distance;
-                target = enemy;
+                _target = enemy;
             }
         }
+    }
+
+    private void Move()
+    {
+        if (_target == null) return;
 
         // 2. 방향을 구한다
-        Vector3 direction = target.transform.position - transform.position;
+        Vector3 direction = _target.transform.position - transform.position;
         direction.y = 0;
         direction.Normalize();
 
+        Vector3 diff = _target.transform.position - transform.position;
+
+        // 적과 나와의 y축 차이가 3보다 크면 앞으로 가고 아니라면 뒤로
+        if (diff.y >= 3)
+        {
+            direction.y += 1;
+        }
+        else
+        {
+            direction.y -= 1;
+        }
+
         // 3. 속도에 맞게 이동한다.
         transform.position += direction * _speed * Time.deltaTime;
+
+        transform.position = new Vector3(transform.position.x,
+            Mathf.Clamp(transform.position.y, _minPlayerY, _maxPlayerY), transform.position.z);
     }
 
     public void SpeedUp(float amount)

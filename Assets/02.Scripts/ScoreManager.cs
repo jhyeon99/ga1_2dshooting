5a603feedf6443aa1a868d2888a8ca2fa9a9a81e
Jae-hyun Song <ccwcs@naver.com>
2026-09-10 13:42:40 +0900

    feat: 스코어 방어 코드 작성
    - score가 0일 시 점수 추가 실패
    - score가 음수일 시 점수 추가 실패

--- Assets/02.Scripts/ScoreManager.cs
+++ Assets/02.Scripts/ScoreManager.cs
@@ -13,6 +13,8 @@ public class ScoreManager : MonoBehaviour
 
     public void AddScore(int score)
     {
+        if (score <= 0) return;
+
         _currentScore += score;
         if (_currentScore > _bestScore)
         {

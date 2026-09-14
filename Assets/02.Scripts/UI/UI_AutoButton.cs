305ab0597a648f6eefe10d40e4b7460766550a01
Jae-hyun Song <ccwcs@naver.com>
2026-09-14 11:06:53 +0900

    feat: 버튼 애니메이션 구현
    - UI_AutoButton내 PlayAnimation함수로 버튼 사이즈 조절 애니메이션
    - OnClick() 연결로 버튼 클릭 시 사용 가능

--- Assets/02.Scripts/UI/UI_AutoButton.cs
+++ Assets/02.Scripts/UI/UI_AutoButton.cs
@@ -16,6 +16,14 @@ public class UI_AutoButton : MonoBehaviour
     private Player _player;
     private bool _autoMode = true;
 
+    [Header("클릭 시 애니메이션")]
+    [SerializeField] private AnimationCurve _bumpCurve;
+
+    private float _scale = 1.0f;
+    private bool _isBumping = false;
+    private float _elapsedTime = 0.0f;
+    private const float BumpDuration = 0.3f;
+    private const float BumpScale = 1.1f;
 
     private void Start()
     {
@@ -35,9 +43,33 @@ public class UI_AutoButton : MonoBehaviour
         _myImage.sprite = _autoMode ? _onSprite : _offSprite;
     }
 
+    public void PlayAnimation()
+    {
+        _isBumping = true;
+        _elapsedTime = 0.0f;
+    }
+
     public void PlaySound()
     {
         _audioSource.Play();
+    }
+
+    private void Update()
+    {
+        if (!_isBumping) return;
+        _elapsedTime += Time.deltaTime;
+        // 1. 경과 시간 누적
+        if (_elapsedTime > BumpDuration) // 시간이 다 지났다면..
+        {
+            transform.localScale = Vector3.one;
+            _isBumping = false;
+            return;
+        }
+
+        // 2. 누적 시간과 애니메잇녀 커브에 따른 스케일 변경
+        float time = _elapsedTime / BumpDuration; // 얼마나 지났는지 퍼센트 (0 ~ 1)
+        float curveValue = _bumpCurve.Evaluate(time); // 퍼센트에 따라 커브 애니메이션 값 추출
+        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one * BumpScale, curveValue);
     }
 
     // Todo: 버튼 클릭할 때 애니메이션 + 사운드 추가
